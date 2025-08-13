282de54840d8220428ca8d77cf99c5ed51361c98
DirectorLord <thelord161203@gmail.com>
2025-08-13 13:01:35 +0300

    did the migration

--- EFCore01/Context/ITIDBContext.cs
+++ EFCore01/Context/ITIDBContext.cs
@@ -6,7 +6,15 @@ namespace EFCore01.Context;
 internal class ITIDBContext : DbContext
 {
     //Student
-    public DbSet<Student> Students { get; set; };
+    public DbSet<Student> Students { get; set; }
+
+    public DbSet<Instructor> instructors { get; set; }
+
+    public DbSet<Department> departments { get; set; }
+
+    public DbSet<Topic> topics { get; set; }
+
+    public DbSet<Course> courses { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("server = .; database = ITI; Trusted_Connection = true; TrustServerCertificate = true");
